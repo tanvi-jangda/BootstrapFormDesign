9e4db47d4418eea9805d21e79a3c0878b3575f14
tanvi-jangda <tanvi.jangda@gmail.com>
2023-05-07 15:36:26 +0530

    Reactive forms implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'BootstrapFormDesign';
+  states:any=["Maharashtra","Gujrat","Uttar Pradesh"];
+
+  constructor(public fbObj:FormBuilder)
+  {
+  }
+  ContactDetailsForm=this.fbObj.group({
+    FName:['',[Validators.required,Validators.pattern(/^[a-zA-Z]*$/)]],
+    LName:['',[Validators.required,Validators.pattern(/^[a-zA-Z]*$/)]],
+    Email:['',[Validators.required,Validators.email]],
+    Phone:['',[Validators.required,Validators.maxLength(10),Validators.pattern(/^[0-9]*$/)]],
+    City:['',[Validators.required , Validators.minLength(4),Validators.pattern(/^[a-zA-Z]*$/)]],
+    State:['',this.stateNameValid],
+    ZipCode:['',[Validators.maxLength(6), Validators.pattern(/^[0-9]*$/)]],
+    Comments:['',[Validators.minLength(30)]]
+  });
+
+  get contactFormControl() {
+    return this.ContactDetailsForm.controls;
+  }
+
+stateNameValid(control:FormControl)
+{
+if(control.value=="Select State")
+return {
+  stateNameInValid:true
+};
+else 
+return {
+  stateNameInvalid:null
+};
+}
+
+
+  btnSubmit()
+  {
+    console.log(this.ContactDetailsForm);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,9 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
